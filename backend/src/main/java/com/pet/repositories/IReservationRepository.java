73e8d0bf85f6a9577d42efad2512c9233f8cbc37
camilaMartin14 <camilamartin1403@gmail.com>
2024-08-28 09:41:04 -0300

    update reservation repository

--- backend/src/main/java/com/pet/repositories/IReservationRepository.java
+++ backend/src/main/java/com/pet/repositories/IReservationRepository.java
@@ -1,10 +1,19 @@
 package com.pet.repositories;
 
-import com.pet.models.Qualification;
+import com.pet.enumerations.AvailableHours;
+import com.pet.enumerations.Day;
+import com.pet.models.PetOwner;
+import com.pet.models.PetSitter;
 import com.pet.models.Reservation;
+import java.util.List;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IReservationRepository extends BaseRepository<Reservation, Long>{
 
+    List <Reservation> findByPetSitterId(Long petSitterId);
+    List <Reservation> findByPetOwnerId(Long petOwnerId);
+    List <Reservation> findByPetId(Long petId);
+    boolean existsByPetSitterAndReservationDayAndReservationHour(PetSitter petSitter, Day reservationDay, AvailableHours reservationHour);
+    boolean existsByPetOwnerAndReservationDayAndReservationHour(PetOwner petOwner, Day appointmentDay, AvailableHours appointmentHour); 
 }
