3d02ed362c3cf603ca53a1a9a4115e922e870e15
camilaMartin14 <camilamartin1403@gmail.com>
2024-08-28 10:04:39 -0300

    create dto for reservations

--- backend/src/main/java/com/pet/dtos/requests/ReservationRequestDTO.java
+++ backend/src/main/java/com/pet/dtos/requests/ReservationRequestDTO.java
@@ -1,5 +1,32 @@
-package com.pet.dtos.requests;
+package com.pet.dtos.request;
 
+import com.pet.enumerations.AvailableHours;
+import com.pet.enumerations.Day;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import jakarta.validation.constraints.*;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReservationRequestDTO {
-    
+    @NotNull(message = "Se requiere el ID del cuidador.")
+    private Long petSitterId;
+    @NotNull(message = "Se requiere el ID del dueño.")
+    private Long petOwnerId;
+    @NotNull(message = "Se requiere el ID de la mascota.")
+    private Long petId;
+    @NotNull(message = "Se requiere la fecha de la reserva.")
+    private Day appointmentDay;
+    @NotNull(message = "Se requiere la hora de la reserva.")
+    private AvailableHours appointmentHour;
+    @Size(max = 120, message = "La descripción de la reserva no debe exceder los 120 caracteres.")
+    private String reservationDescription;
 }
