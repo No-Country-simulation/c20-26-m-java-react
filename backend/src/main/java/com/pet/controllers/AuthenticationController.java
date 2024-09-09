ac8d707c33bc255cb207c04fbd2adab65a634683
ArroyavePJuan <jarroyavepa@cesde.net>
2024-09-09 16:38:18 -0500

    Commit antes de pruebas

--- backend/src/main/java/com/pet/controllers/AuthenticationController.java
+++ backend/src/main/java/com/pet/controllers/AuthenticationController.java
@@ -27,15 +27,14 @@ public class AuthenticationController {
     public String createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try {
             authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+                    new UsernamePasswordAuthenticationToken("Juan", "123")
             );
         } catch (Exception e) {
             throw new Exception("Incorrect username or password", e);
         }
 
         final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-        final String jwt = jwtUtil.generateToken(userDetails.getUsername());
 
-        return jwt;
+        return jwtUtil.generateToken(userDetails.getUsername());
     }
 }
