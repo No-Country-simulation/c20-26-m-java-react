b98b46c1f1bfda7dd6a393ea7c618d72b891aafe
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-09-09 16:39:39 -0300

    remove

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
 
 
--- frontend/src/pages/pageUser/indexUser.jsx
+++ frontend/src/pages/pageUser/indexUser.jsx
@@ -7,6 +7,7 @@ export default function User() {
         <div>
             <CardProfile/>
             <Card/>
+            
         </div>
             
     )
