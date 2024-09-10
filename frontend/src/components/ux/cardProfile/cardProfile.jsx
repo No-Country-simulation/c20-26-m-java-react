7cbbd5ce4173a42ab399c6a270a081891cd7e664
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-09-10 14:30:36 -0300

    se arreglan detales de los componentes

--- frontend/src/components/ux/cardProfile/cardProfile.jsx
+++ frontend/src/components/ux/cardProfile/cardProfile.jsx
@@ -9,7 +9,7 @@ export default function CardProfile(){
                         <div className="mx-1 px-1 "> 
                             <div className="text-center">
                                 <div className="circle-image">
-                                    <img src="../src/assets/images/pet.jpg" alt="Mascota" />
+                                    <img src="../src/assets/images/pet1.jpg" alt="Mascota" />
                                 </div>
                                 <div className="card-body bg-c1 border-radius ">
                                     <h5 className="card-title ">Coco</h5>
@@ -52,6 +52,34 @@ export default function CardProfile(){
                             <div className="text-center">
                                 <div className="circle-image">
                                     <img src="../src/assets/images/pet4.jpg" alt="Mascota" />
+                                </div>
+                                <div className="card-body bg-c1 border-radius">
+                                    <h5 className="card-title">Coco</h5>
+                                    <p className="card-text p6">Labrador - 2 años</p>
+                                    <p className="card-text p6">Laura Rodriguez</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row m-2">
+                        <div className="mx-1 px-1 mb-2 custom-margin"> 
+                            <div className="text-center">
+                                <div className="circle-image">
+                                    <img src="../src/assets/images/pet5.jpg" alt="Mascota" />
+                                </div>
+                                <div className="card-body bg-c1 border-radius">
+                                    <h5 className="card-title">Coco</h5>
+                                    <p className="card-text p6">Labrador - 2 años</p>
+                                    <p className="card-text p6">Laura Rodriguez</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row m-2">
+                        <div className="mx-1 px-1 mb-2 custom-margin"> 
+                            <div className="text-center">
+                                <div className="circle-image">
+                                    <img src="../src/assets/images/pet6.jpg" alt="Mascota" />
                                 </div>
                                 <div className="card-body bg-c1 border-radius">
                                     <h5 className="card-title">Coco</h5>
--- frontend/src/pages/pageUser/indexUser.jsx
+++ frontend/src/pages/pageUser/indexUser.jsx
@@ -6,29 +6,29 @@ import Calendar from "../../components/ux/calendar/calendar"
 export default function User() {
     return(
         <div>
-           <nav className="">
-                <div className="">
+           <nav>
+                <div>
                     <form className="d-flex" role="search">
-                    <input className="form-control me-2 border-radius" type="search" placeholder="Busca tu servicio favorito" aria-label="Search"/>
-                    <button className="btn bg-c1 border-radius" type="submit">Search</button>
+                        <input className="form-control me-2 border-radius" type="search" placeholder="Busca tu servicio favorito" aria-label="Search"/>
+                        <button className="btn bg-c1 border-radius" type="submit">Search</button>
                     </form>
                 </div>
                 <nav className="navbar">
-                    <div className="container-fluid">
-                        <h2 className="navbar mt-2" href="#">Bienvenida, Julieta!</h2>
-                    </div>
+                    <h2 className="navbar mt-2 m-1" href="#">Bienvenida, Julieta!</h2>
                 </nav>
             </nav>
-            <div className="mt-5 text-center">
+            <div className="mt-5">
+                
                 <div className="row">
-                    <div className="col">
+                    <h4 className="">Tus clientes</h4>
+                    <div className="col-8 mt-3">
                         <CardProfile/>
                     </div>
-                    <div className="col">
+                    <div className="col-4">
                         <Calendar/>
                     </div>
                 </div>
             </div>
-        </div>    
+        </div>
     )
 }
