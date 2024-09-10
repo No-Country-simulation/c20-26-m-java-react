815b89790c9d929e6d02a0893d4a707ba4ab4afb
camilaMartin14 <camilamartin1403@gmail.com>
2024-09-10 11:28:08 -0300

    start implementing roles and permissions

--- backend/src/main/java/com/pet/config/SecurityConfig.java
+++ backend/src/main/java/com/pet/config/SecurityConfig.java
@@ -2,7 +2,11 @@ package com.pet.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -21,5 +25,18 @@ public class SecurityConfig {
             .sessionManagement(session -> 
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .build();
+    }
+    
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfiguration) throws Exception{
+        return authConfiguration.getAuthenticationManager();
+    }
+    
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(null);
+        provider.setUserDetailsService(null);
+        return provider;
     }
 }
