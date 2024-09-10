7ed7497ffbd380cae124a55025542e1c26bb1ecd
Julieta Aguilera <aguilerajulieta700@gmail.com>
2024-09-10 11:32:38 -0300

    se agregan cambios

--- frontend/src/components/ux/cardEvent/cardEvent.jsx
+++ frontend/src/components/ux/cardEvent/cardEvent.jsx
@@ -2,16 +2,9 @@
 
 export default function CardEvent() {
     return(
-<<<<<<< HEAD
         <div className="container">
             <div className="card bg-c2 border-radius mb-3" style={{ maxWidth: "18rem" }}>
-                <div className="card-header text-center">Agosto 15</div>
-=======
-        
-        <div className="container">
-            <div className="card mb-3 bg-c2 border-radius" style={{ maxWidth: "18rem" }}>
-                <div className="card-header text-center">Eventos</div>
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
+                <div className="card-header text-center">Eventos pendientes</div>
                 <div className="card-body">
                     {/* Primera tarjeta */}
                     <div className="card mb-3 border-radius">
@@ -22,11 +15,7 @@ export default function CardEvent() {
                             <div className="d-flex flex-column">
                                 <span className="fw-bold">Maylo</span>
                                 <span>Pastor Aleman</span>
-<<<<<<< HEAD
-                                <span className="text-muted">09:00 pm</span>
-=======
                                 <span className="text-muted">14/09</span>
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
                             </div>
                         </div>
                     </div>
@@ -39,11 +28,7 @@ export default function CardEvent() {
                             <div className="d-flex flex-column">
                                 <span className="fw-bold">Maylo</span>
                                 <span>Pastor Aleman</span>
-<<<<<<< HEAD
-                                <span className="text-muted">09:00 pm</span>
-=======
                                 <span className="text-muted">25/09</span>
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
                             </div>
                         </div>
                     </div>
@@ -51,29 +36,17 @@ export default function CardEvent() {
                     <div className="card mb-3 border-radius">
                         <div className="container d-flex flex-row align-items-center">
                             <div className="me-3">
-<<<<<<< HEAD
-                                <i className="bi bi-clipboard-check-fill fs-3 "></i>
-=======
                                 <i className="bi bi-clipboard-check-fill fs-3"></i>
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
                             </div>
                             <div className="d-flex flex-column">
                                 <span className="fw-bold">Maylo</span>
                                 <span>Pastor Aleman</span>
-<<<<<<< HEAD
-                                <span className="text-muted">09:00 pm</span>
-=======
                                 <span className="text-muted">02/10</span>
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> c4afc4de7c4e25f115b90ecd2857f9ba5807f870
     )
 }
