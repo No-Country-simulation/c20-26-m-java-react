2e0c27dad64e7374c82cf19d4c7619d484edc5d2
camilaMartin14 <camilamartin1403@gmail.com>
2024-09-10 22:03:11 -0300

    complete the implementation of roles and permissions

--- backend/src/main/java/com/pet/config/SecurityConfig.java
+++ backend/src/main/java/com/pet/config/SecurityConfig.java
@@ -1,7 +1,10 @@
 package com.pet.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -10,6 +13,11 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -17,13 +25,18 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http
-            .csrf(customizer -> customizer.disable())
-            .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+            .csrf(customizer -> customizer.disable())    
             .httpBasic(Customizer.withDefaults())
             .sessionManagement(session -> 
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                        .authorizeHttpRequests(request -> {
+                    //request.anyRequest().authenticated())
+                    request.requestMatchers(HttpMethod.GET, "/api/pet-services").permitAll();
+                    request.requestMatchers(HttpMethod.GET, "/api/reservations/getAll").hasAuthority("READ");
+                    request.anyRequest().denyAll();
+                        })
             .build();
     }
     
@@ -35,8 +48,32 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(null);
-        provider.setUserDetailsService(null);
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
         return provider;
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+    
+    @Bean
+    public UserDetailsService userDetailsService(){
+        List userDetailsList = new ArrayList<>();
+        
+        userDetailsList.add(User.withUsername("PatitasCEO")
+        .password("1234")
+        .roles("ADMIN")
+        .authorities("CREATE", "READ", "UPDATE", "DELETE")
+        .build());
+        
+        userDetailsList.add(User.withUsername("PatitasAdministrador")
+        .password("1234")
+        .roles("USER")
+        .authorities("UPDATE", "READ")
+        .build());
+        
+        return new InMemoryUserDetailsManager(userDetailsList);
     }
 }
