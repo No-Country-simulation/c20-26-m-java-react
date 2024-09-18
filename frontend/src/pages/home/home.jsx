0c0fa247b334827242884faaae16b85d5522699c
MaximilianoTRZ <maximilianoriverozuin@gmail.com>
2024-09-18 13:43:47 -0300

    home page added

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./home.scss";
+import dogImage from "../../assets/images/Image.png";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home-container">
+      <div className="left-section">
+        <h1>Plataforma de cuidado de mascotas</h1>
+      </div>
+      <div className="right-section">
+        <img src={dogImage} alt="Dog" />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
